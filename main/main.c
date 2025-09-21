8eda055d0c702a8544b173dfa339b74564a1f160
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-09-21 18:26:12 +0100

    feat: create a parser

--- main/main.c
+++ main/main.c
@@ -80,5 +80,161 @@ void printAST(ASTNode *node, int depth) {
 
 //lexer 
 
+int isKeyWord(const char *str) {
+    return strcmp(str, "let") == 0;
+}
+
+Token getNextToken(FILE *fp) {
+
+    Token token;
+    int c;
+    int i = 0;
+
+    while ((c == fgetc(fp)) != EOF && isspace(c));
+    
+    if(c == EOF) {
+        token.type = TOKEN_EOF;
+        strcpy(token.text, "EOF");
+        return token;
+    }
+
+    if(isalpha(c) || c == '_') {
+
+        token.text[i++] = c;
+        while ((c == fgetc(fp)) != EOF && (isalnum(c) || c == '_')) {
+            token.text[i++] = c;
+        }
+        token.text[i] = '\0';
+        ungetc(c, fp);
+
+        if( isKeyWord(token.text) ) {
+            token.type = TOKEN_LET;
+        } else {
+            token.type = TOKEN_IDENTIFIER;
+        }
+        return token;
+
+    }
+
+    if(isdigit(c)) {
+        token.text[i++] = c;
+        while ((c = fgetc(fp)) != EOF && isdigit(c)){
+            token.text[i++] = c;
+        }
+
+        token.text[i] = '\0';
+        ungetc(c, fp);
+        token.type = TOKEN_NUMBER;
+        return token;
+        
+    }
+
+    if(c == '=') {
+        token.type = TOKEN_ASSIGN;
+        strcpy(token.text, '=');
+        return token;
+    }
+
+    if( c == ';' ){
+        token.type = TOKEN_SEMICOLON;
+        strcpy(token.text, ';');
+        return token;
+    }
+
+    token.type = TOKEN_UNKNOW;
+    token.text[0] = c;
+    token.text[1] = '\0';
+    return token;
+
+}
+
+
+// Parser
+
+Token currentToken;
+FILE *fp;
+
+void advance() {
+
+    currentToken = getNextToken(fp);
+
+}
+
+
+void expect(TokenType type) {
+
+    if( currentToken.type != type ){
+
+        printf("Erro de sintaxe: esperado token %d mas veio %d (%s)\n", type, currentToken.type, currentToken.text);
+        exit(1);
+
+    }
+    advance();
+
+}
+
+//Parse ex: let x = 10;
+
+ASTNode *parseVariableDeclaration() {
+
+    expect(TOKEN_LET);
+
+    if(currentToken.type != TOKEN_IDENTIFIER) {
+        printf("Erro: esperado identificador após 'let'\n");
+        exit(1);
+    }
+
+    ASTNode *idNode = createNote(AST_IDENTIFIER, currentToken.text);
+    advance();
+
+    expect(TOKEN_ASSIGN);
+
+    if(currentToken.type != TOKEN_NUMBER) {
+        printf("Erro: esperado número após '='\n");
+        exit(1);
+    }
+
+    ASTNode *numNode = createNote(AST_NUMBER, currentToken.text);
+    advance();
+
+    expect(TOKEN_SEMICOLON);
+    ASTNode *varDecl = createNote(AST_VARIABLE_DECL, "");
+    varDecl->left = idNode;
+    varDecl->right = numNode;
+
+    return varDecl;
+
+}
+
+
+
+
+// Main da aplicação
+
+int main() {
+
+    fp = fopen("input.ts", "r");
+    if(!fp){
+
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
+
+    }
+
+    advance();
+    ASTNode *tree = parseVariableDeclaration();
+
+    printf("===== AST =====\n");
+    printAST(tree, 0);
+    
+    fclose(fp);
+    return 0;
+
+}
+
+
+
+
+
 
 
